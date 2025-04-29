8811797db2c5d36b76895e3bb50459d04f0ddc29
ezraeffect <build@fx0.kr>
2025-04-29 11:38:17 +0900

    add:div number function

--- day7_1/Form1.cs
+++ day7_1/Form1.cs
@@ -16,6 +16,20 @@ namespace day7_1
         {
             InitializeComponent();
 
+            int a = 8;
+            int b = 3;
+            int[] result = new int[2];
+            result = DivNumber(a, b);
+
+            textBox_print.Text = "몫 : " + result[0].ToString() + " / 나머지 : " + result[1].ToString();
         }
+
+        int[] DivNumber(int a, int b){
+            int[] result = new int[2];
+            result[0] = a / b;
+            result[1] = a % b;
+            return result;
+        }
+
     }
 }
