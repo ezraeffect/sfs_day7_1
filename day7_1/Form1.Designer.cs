1bc6f071ebb23e845b0599368eeea4f93661abf4
ezraeffect <build@fx0.kr>
2025-04-29 09:03:21 +0900

    add:textBox

--- day7_1/Form1.Designer.cs
+++ day7_1/Form1.Designer.cs
@@ -28,13 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox_print = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox_print
+            // 
+            this.textBox_print.Font = new System.Drawing.Font("굴림", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox_print.Location = new System.Drawing.Point(12, 12);
+            this.textBox_print.Multiline = true;
+            this.textBox_print.Name = "textBox_print";
+            this.textBox_print.Size = new System.Drawing.Size(776, 426);
+            this.textBox_print.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox_print);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox_print;
     }
 }
 
